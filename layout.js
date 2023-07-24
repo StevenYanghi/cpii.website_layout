20c95ed2e9e29fb0c060b9f49e43fa1ad1f2f7c9
StevenYanghi <137042229+StevenYanghi@users.noreply.github.com>
2023-07-24 09:15:11 +0800

    Add files via upload

--- layout.js
+++ layout.js
@@ -16,16 +16,6 @@ for (var i = 0; i < menuItems.length; i++) {
   }
 }
 
-// Add "active" class to the current menu item
-var currentLocation = window.location.href;
-var menuItems = document.querySelectorAll('nav a');
-
-for (var i = 0; i < menuItems.length; i++) {
-  if (menuItems[i].href === currentLocation) {
-    menuItems[i].classList.add('active');
-  }
-}
-
 // Add "open" class to the current pull-down menu item
 var menuItemsWithSubMenu = document.querySelectorAll('nav li ul li a');
 

2dbf58b9bd4ea75ce002b4e59095382ff3ce994c
StevenYanghi <137042229+StevenYanghi@users.noreply.github.com>
2023-07-27 09:20:35 +0800

    Add files via upload

--- layout.js
+++ layout.js
@@ -37,3 +37,41 @@ window.addEventListener('scroll', () => {
     stickyList.classList.remove('sticky');
   }
 });
+
+(function flexible(window, document) {
+  const doc = document.documentElement;
+  const dpr = window.devicePixelRatio || 1;
+
+  // 适配 body 字体大小
+  function setBodyFontSize() {
+    if (document.body) {
+      document.body.style.fontSize = 12 * dpr + 'px';
+    } else {
+      document.addEventListener('DOMContentLoaded', setBodyFontSize);
+    }
+  }
+  setBodyFontSize();
+
+
+  // 页面容器伸缩时重置 rem 单位
+  window.addEventListener('resize', setRemUnit);
+  window.addEventListener('pageshow', function (err) {
+    if (e.persisted || (window.performance && window.performance.navigation.type === 2)) {
+      setRemUnit();
+    }
+  });
+
+  // 检测是否支持 0.5 px（用于解决 1px 问题）
+  if (dpr >= 2) {
+    const fakeBody = document.createElement('body');
+    const div = document.createElement('div');
+
+    div.style.border = '.5px solid transparent';
+    fakeBody.appendChild(div);
+    doc.appendChild(fakeBody);
+
+    if (div.offsetHeight === 1) {
+      doc.classList.add('hairlines');
+    }
+  }
+})(window, document);
